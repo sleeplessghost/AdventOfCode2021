bb7fa808dde74eaf8db6b0ac1e3902df7cd94738
sleeplessghost <sleeplessghost@live.com>
2021-12-08 03:33:14 +1030

    tidying

--- 07.py
+++ 07.py
@@ -6,7 +6,7 @@ def basicFuel(nums, pos):    return sum(abs(n - pos) for n in nums)
 def advancedFuel(nums, pos): return sum(sumToN(abs(n - pos)) for n in nums)
 
 def calcFuel(nums, fuelFunc):
-    return min([fuelFunc(nums, pos) for pos in range(min(nums), max(nums) + 1)])
+    return min(fuelFunc(nums, pos) for pos in range(min(nums), max(nums) + 1))
 
 nums = [int(n) for n in open('in/07.txt').read().split(',')]
 print('part1:', calcFuel(nums, basicFuel))
