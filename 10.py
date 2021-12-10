c2512baf037ae740ebb720cd3d878e55c7fe6a19
sleeplessghost <sleeplessghost@live.com>
2021-12-10 16:22:34 +1030

    tidying

--- 10.py
+++ 10.py
@@ -1,50 +1,26 @@
-from collections import deque
-from math import floor
+from collections import deque, namedtuple
+from functools import reduce
 
-def corruptScore(line):
+closingTags = { '(':')', '[':']', '{':'}', '<':'>' }
+corruptValues = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+closingValues = { ')': 1, ']': 2, '}': 3, '>': 4 }
+score = namedtuple('score', ['completion', 'corrupt'])
+
+def lineScore(line):
     q = deque()
     for c in line:
         if c in ['(', '[', '{', '<']: q.append(c)
-        else:
-            current = q.pop()
-            if c != closingTag(current): return corruptVal(c)
-    return 0
+        elif c != closingTags[q.pop()]: return score(0, corruptValues[c])
+    return score(completionScore(q), 0)
 
-def completeLine(line):
-    q = deque()
-    for c in line:
-        if c in ['(', '[', '{', '<']: q.append(c)
-        else: q.pop()
-    score = 0
-    while(q): score = score * 5 + closingVal(closingTag(q.pop()))
-    return score
-
-def closingTag(c):
-    match c:
-        case '[': return ']'
-        case '(': return ')'
-        case '{': return '}'
-        case '<': return '>'
-
-def corruptVal(c):
-    match c:
-        case ')': return 3
-        case ']': return 57
-        case '}': return 1197
-        case '>': return 25137
-
-def closingVal(c):
-    match c:
-        case ')': return 1
-        case ']': return 2
-        case '}': return 3
-        case '>': return 4
+def completionScore(queue):
+    queue.reverse()
+    return reduce(lambda currentScore, c: currentScore * 5 + closingValues[closingTags[c]], queue, 0)
 
 lines = [x.strip() for x in open('in/10.txt')]
-corruptScores = [corruptScore(x) for x in lines]
-incomplete = [x for x in lines if corruptScore(x) == 0]
-incompleteScores = [completeLine(x) for x in incomplete]
-incompleteScores.sort()
+scores = [lineScore(x) for x in lines]
+completionScores = [s.completion for s in scores if s.completion > 0]
+completionScores.sort()
 
-print('part1:', sum(corruptScores))
-print('part2:', incompleteScores[floor(len(incompleteScores) / 2)])
+print('part1:', sum(s.corrupt for s in scores))
+print('part2:', completionScores[len(completionScores) // 2])
