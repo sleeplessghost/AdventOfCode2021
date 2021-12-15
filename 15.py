e5c4b90c827f93e4c781b847da74fea2ee18967b
sleeplessghost <sleeplessghost@live.com>
2021-12-16 08:42:17 +1030

    more tidying

--- 15.py
+++ 15.py
@@ -1,4 +1,5 @@
 from heapq import heappop, heappush
+from collections import defaultdict
 import numpy as np
 
 def neighbours(x, y):
@@ -7,37 +8,27 @@ def neighbours(x, y):
 def getMap(inputs):
     return {(x,y): inputs[y][x] for x in range(len(inputs[0])) for y in range(len(inputs))}
 
-def getRisks(mapped):
-    risks, q = {}, []
-    heappush(q, (0, (0,0)))
+def lowestRisk(mapped):
+    target = max(mapped)
+    q, visited = [(0, (0,0))], defaultdict(bool)
+    visited[(0,0)] = True
     while q:
         risk, point = heappop(q)
-        if risk not in risks or risk < risks[point]:
-            risks[point] = risk
-            nextPoints = [n for n in neighbours(*point) if n in mapped]
-            for n in nextPoints:
-                r = risk + mapped[n]
-                if n not in risks or r < risks[n]:
-                    risks[n] = r
-                    heappush(q, (r, n))
-    return risks
+        if point == target: return risk
+        nextPoints = [n for n in neighbours(*point) if n in mapped and not visited[n]]
+        for n in nextPoints:
+            visited[n] = True
+            heappush(q, (risk + mapped[n], n))
 
 def makeBigger(inputs):
     LX, LY = len(inputs[0]), len(inputs)
     tiled = np.tile(inputs, (5, 5))
-    for y in range(5 * LY):
-        for x in range(5 * LX):
-            if x >= LX or y >= LY:
-                tiled[y][x] += (x // LX) + (y // LY)
-                if tiled[y][x] > 9: tiled[y][x] %= 9
-    return tiled
-
-def solve(input):
-    mapped = getMap(input)
-    risks = getRisks(mapped)
-    return risks[(max(x for x,y in mapped), max(y for x,y in mapped))]
+    return [[(tiled[y][x] + x//LX + y//LY -1) % 9 + 1 for x in range(5 * LX)] for y in range(5 * LY)]
 
 input = [[int(n) for n in x.strip()] for x in open('in/15.txt')]
 bigInput = makeBigger(input)
-print('part1:', solve(input))
-print('part2:', solve(bigInput))
+
+mapped = getMap(input)
+print('part1:', lowestRisk(mapped))
+mapped = getMap(bigInput)
+print('part2:', lowestRisk(mapped))
