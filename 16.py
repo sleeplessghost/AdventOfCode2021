58fc114886cebd1ab28574edcfea851b8ab9de17
sleeplessghost <sleeplessghost@live.com>
2021-12-17 06:24:14 +1030

    tidying

--- 16.py
+++ 16.py
@@ -1,55 +1,50 @@
 from math import prod
 
 class Packet:
-    def __init__(self, version, id, value, subpackets):
-        self.version = version
-        self.id = id
-        self.value = value
-        self.subpackets = subpackets
+    def __init__(self, version, id, literal, subpackets):
+        self.version, self.id, self.literal, self.subpackets = version, id, literal, subpackets
 
     def sumVersions(self):
         return self.version + sum(p.sumVersions() for p in self.subpackets)
     
     def getValue(self):
-        subValues = [p.getValue() for p in self.subpackets]
+        subValues = (p.getValue() for p in self.subpackets)
         match self.id:
             case 0: return sum(subValues)
             case 1: return prod(subValues)
             case 2: return min(subValues)
             case 3: return max(subValues)
-            case 4: return self.value
-            case 5: return 1 if self.subpackets[0].getValue() > self.subpackets[1].getValue() else 0
-            case 6: return 1 if self.subpackets[0].getValue() < self.subpackets[1].getValue() else 0
-            case 7: return 1 if self.subpackets[0].getValue() == self.subpackets[1].getValue() else 0
+            case 4: return self.literal
+            case 5: return self.subpackets[0].getValue() > self.subpackets[1].getValue()
+            case 6: return self.subpackets[0].getValue() < self.subpackets[1].getValue()
+            case 7: return self.subpackets[0].getValue() == self.subpackets[1].getValue()
 
-    @staticmethod
-    def parsePacket(binary):
-        version, binary = int(binary[:3], 2), binary[3:]
-        id, binary = int(binary[:3], 2), binary[3:]
-        if id == 4: #value packet
-            chunks = [binary[i:i+5] for i in range(0, len(binary), 5)]
-            endIndex = next(i for i,chunk in enumerate(chunks) if chunk[0] == '0')
-            binary = ''.join(chunks[endIndex+1:])
-            value = int(''.join([chunk[1:] for chunk in chunks[:endIndex+1]]), 2)
-            return Packet(version, id, value, []), binary
-        else: #operator packet
-            subpackets = []
-            length, binary = binary[0], binary[1:]
-            if length == '0':
-                totalLength, binary = int(''.join(binary[:15]), 2), binary[15:]
-                sub, binary = binary[:totalLength], binary[totalLength:]
-                while sub:
-                    packet, sub = Packet.parsePacket(sub)
-                    subpackets.append(packet)
-            else:
-                numSubs, binary = int(''.join(binary[:11]), 2), binary[11:]
-                for _ in range(numSubs):
-                    packet, binary = Packet.parsePacket(binary)
-                    subpackets.append(packet)
-            return Packet(version, id, 0, subpackets), binary
+def parsePacket(binary):
+    version, binary = int(binary[:3], 2), binary[3:]
+    id, binary = int(binary[:3], 2), binary[3:]
+    if id == 4: #value packet
+        chunks = [binary[i:i+5] for i in range(0, len(binary), 5)]
+        endIndex = next(i for i,chunk in enumerate(chunks) if chunk[0] == '0')
+        binary = ''.join(chunks[endIndex+1:])
+        literal = int(''.join([chunk[1:] for chunk in chunks[:endIndex+1]]), 2)
+        return Packet(version, id, literal, []), binary
+    else: #operator packet
+        length, binary, subpackets = binary[0], binary[1:], []
+        if length == '0':
+            totalLength, binary = int(binary[:15], 2), binary[15:]
+            sub, binary = binary[:totalLength], binary[totalLength:]
+            while sub:
+                packet, sub = parsePacket(sub)
+                subpackets.append(packet)
+        else:
+            numSubs, binary = int(binary[:11], 2), binary[11:]
+            for _ in range(numSubs):
+                packet, binary = parsePacket(binary)
+                subpackets.append(packet)
+        return Packet(version, id, 0, subpackets), binary
 
 hexi = open('in/16.txt').read()
 binary = ''.join([bin(int(c, 16))[2:].zfill(4) for c in hexi])
-packet, _ = Packet.parsePacket(binary)
+packet, _ = parsePacket(binary)
 print('part1:', packet.sumVersions())
 print('part2:', packet.getValue())
